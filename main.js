bf0cc164a0babe37d51d5bb3aa6070aa92e64ad5
Karina Zhuro <kzhuro36@gmail.com>
2019-07-24 13:40:10 +0300

    Test

--- main.js
+++ main.js
@@ -3,19 +3,28 @@ var inputY1 = document.querySelector('.inputY1');
 var inputX2 = document.querySelector('.inputX2');
 var inputY2 = document.querySelector('.inputY2');
 
-// var rook1 = document.querySelector('.rook1');
-// var elep1 = document.querySelector('.elep1');
-// var rook2 = document.querySelector('.rook2');
-// var rook2 = document.querySelector('.rook2');
-
 var resultX = document.querySelector('.resultX');
 var resultY = document.querySelector('.resultY');
 
-var coordX = Number(inputX1.value);
-var coordY = Number(inputY1.value);
-
 var btn = document.querySelector('.btn');
 
-btn.addEventListener('click', function (event) {
-    resultX.textContent = 'x = ' + inputX1.value + ', ' + 'y = ' + inputY1.value;
-});
+var radio = document.getElementsByName('customRadio');
+for (var i = 0; i < radio.length; i++) {
+    radio[i].onchange = testRadio;
+}
+function testRadio() {
+    console.log(.value);
+}
+document.getElementsByClassName('btn').onclick = checkRadio;
+function checkRadio() {
+    var m = document.getElementsByName('customRadio');
+    for (var i = 0; i < m.length; i++) {
+        if (m[i].checked) {
+            resultX.textContent = m[i].value + ': x = ' + inputX1.value + '; ' + 'y = ' + inputY1.value;
+            break;
+        }
+    }
+}
+// btn.addEventListener('click', function (event) {
+//     resultY.textContent = 'x = ' + inputX2.value + '; ' + 'y = ' + inputY2.value;
+// });
